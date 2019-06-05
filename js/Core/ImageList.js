69eb04ffc30199033fd658a68db3a2767bb568d8
rpan1ocdsb <51001302+rpan1ocdsb@users.noreply.github.com>
2019-06-05 15:12:20 -0400

    Update ImageList.js

--- js/Core/ImageList.js
+++ js/Core/ImageList.js
@@ -1,4 +1,4 @@
 //add image sources here 
 imageList.push(
-	"tree/master/Images/a.png", "Images/book.png", "Images/eye.png", "Images/angel.png", "Images/bee.png", "Images/chest.png", "Images/diamond.png", "Images/spiderman.png", "Images/shield.png", 
+	"blob/master/Images/a.png", "Images/book.png", "Images/eye.png", "Images/angel.png", "Images/bee.png", "Images/chest.png", "Images/diamond.png", "Images/spiderman.png", "Images/shield.png", 
 )
